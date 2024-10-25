24d59b1986af317659ba45027e3718e89ecb96de
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2024-10-25 16:56:55 +0200

    dev

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import Recipe from './Recipe';
+import React, { useState } from 'react';
+import './RecipeList.css';
 
-const RecipeList = ({ recipes }) => (
-  <div className="recipe-list">
-    {recipes.map((recipe, index) => (
-      <Recipe
-        key={index}
-        title={recipe.title}
-        ingredients={recipe.ingredients}
-        instructions={recipe.instructions}
-        comments={recipe.comments}
-      />
-    ))}
-  </div>
-);
+const RecipeList = ({ recipes }) => {
+  const [visibleRecipes, setVisibleRecipes] = useState({});
+
+  const toggleVisibility = (index) => {
+    setVisibleRecipes(prevState => ({
+      ...prevState,
+      [index]: !prevState[index]
+    }));
+  };
+
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe, index) => (
+        <div key={index} className="recipe">
+          <h2 onClick={() => toggleVisibility(index)}>{recipe.title}</h2>
+          {visibleRecipes[index] && (
+            <div className="recipe-details">
+              <h3>Zutaten</h3>
+              <ul>
+                {recipe.ingredients.split('\n').map((ingredient, i) => (
+                  <li key={i}>{ingredient}</li>
+                ))}
+              </ul>
+              <h3>Zubereitung</h3>
+              <p>{recipe.instructions}</p>
+              {recipe.comments && (
+                <>
+                  <h3>Kommentar</h3>
+                  <p>{recipe.comments}</p>
+                </>
+              )}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default RecipeList;
