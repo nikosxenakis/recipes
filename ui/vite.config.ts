3a729568cb68785601064f1df444bf96a24d48c1
Nikolaos Xenakis <n.xenakis@sphynx.ch>
2025-07-25 12:44:32 +0200

    minor updates

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import string from 'vite-plugin-string';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import string from "vite-plugin-string";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    string({
-      include: ['**/*.md'],
-    }),
-  ],
-})
+    plugins: [
+        react(),
+        string({
+            include: ["**/*.md"],
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules/react") || id.includes("node_modules/react-dom")) {
+                        return "react";
+                    }
+                    if (id.includes("Rezeptbuch.md")) return "rezeptbuch";
+                },
+            },
+        },
+    },
+});
